c8c10700e11b70f6b49ea117d2a12ab80e868778
carry1111 <529709962@qq.com>
2018-12-19 01:31:54 +0800

    pages

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <merchantIntroduction></merchantIntroduction>
+  </div>
+</template>
+
+<script>
+import merchantIntroduction from '@/components/Home/merchant_introduction'
+export default {
+  name: "home",
+  components: {
+    merchantIntroduction
+  }
+};
+</script>
