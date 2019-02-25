ca356cccb2b6a7169f1ce241aa4f825763d6fbaa
carry1111 <529709962@qq.com>
2019-02-26 02:35:57 +0800

    已合并

--- src/main.js
+++ src/main.js
@@ -4,18 +4,12 @@ import router from "./router";
 import store from "./store";
 
 import '@/assets/css/reset.css'; 
-<<<<<<< HEAD
 import '@/assets/css/common.css';
 
 Vue.config.productionTip = false;
 
 require('./mock')
 
-=======
-
-Vue.config.productionTip = false;
-
->>>>>>> 9aa6a9895a647ce05f11f4dce9d9d5357a858748
 new Vue({
   router,
   store,
